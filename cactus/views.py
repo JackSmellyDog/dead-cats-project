d753338715885dd60c2a1a1ee25ca6b5733dc621
Vladyslav Velychko <velyk95@gmail.com>
2017-05-13 21:19:22 +0300

    add instagram api

--- cactus/views.py
+++ cactus/views.py
@@ -1,32 +1,59 @@
 import tweepy, json
 from tweepy import OAuthHandler
+from instaLooter import InstaLooter
+import pdb
 
-consumer_key = 'bJbCKR9bnywpupwE20x16glPk'
-consumer_secret = 'bstcGDbEZVAmOdeQGVwo9GQpfR6x7vFIqsV3ggfUA4mKwCweQ1'
-access_token = '1198023481-nm5bdPvTV5j4qY5svmYjgGarPrPNMwEuGRRJCRH'
-access_secret = 'KuaSE62266vSm8EYlyOiOi4iOvaCssv7gclCW12eolglY'
 
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
+def twitter():
+    consumer_key = 'bJbCKR9bnywpupwE20x16glPk'
+    consumer_secret = 'bstcGDbEZVAmOdeQGVwo9GQpfR6x7vFIqsV3ggfUA4mKwCweQ1'
+    access_token = '1198023481-nm5bdPvTV5j4qY5svmYjgGarPrPNMwEuGRRJCRH'
+    access_secret = 'KuaSE62266vSm8EYlyOiOi4iOvaCssv7gclCW12eolglY'
 
-api = tweepy.API(auth)
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
 
-#def process_or_store(tweet):
-    #print(json.dumps(tweet))
+    api = tweepy.API(auth)
 
-#for tweet in tweepy.Cursor(api.user_timeline).items():
-    #process_or_store(tweet._json)
+    #def process_or_store(tweet):
+        #print(json.dumps(tweet))
 
-#for status in tweepy.Cursor(api.home_timeline).items(10):
-    #print(status.text)
+    #for tweet in tweepy.Cursor(api.user_timeline).items():
+        #process_or_store(tweet._json)
 
-#for status in tweepy.Cursor(api.home_timeline).items(10):
-    # Process a single status
-    #process_or_store(status._json)
+    #for status in tweepy.Cursor(api.home_timeline).items(10):
+        #print(status.text)
 
-#for friend in tweepy.Cursor(api.friends).items():
-    #process_or_store(friend._json)
+    #for status in tweepy.Cursor(api.home_timeline).items(10):
+        # Process a single status
+        #process_or_store(status._json)
 
-#if __name__=="__main__":
-user = api.get_user(input("Please enter the twitter username: "))
-print(api.me().name)
+    #for friend in tweepy.Cursor(api.friends).items():
+        #process_or_store(friend._json)
+
+    #if __name__=="__main__":
+    user = api.get_user(input("Please enter the twitter username: "))
+    return (api.me().name)
+    return (api.me().description)
+
+
+def instagram():
+    k = 0
+
+    username = input("Input username: ")
+
+    looter = InstaLooter(profile=username)
+
+    # with open("outimagesfile.txt", "w") as output:
+    # pdb.set_trace()
+    for media in looter.medias():
+        url = looter.get_post_info(media['code']['edge_media_to_caption'])
+        k += 1
+        if k > 2:
+            break
+            # else:
+            # url = media['display_src']
+            # k+=1
+        # if k==3:
+        # break output.write
+        print("{}\n".format(url))

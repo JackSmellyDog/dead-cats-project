5a931ec5d623939f3485b68a59d80be3fe09b54a
Vladyslav Velychko <velyk95@gmail.com>
2017-05-13 21:35:41 +0300

    small bug fixes

--- cactus/views.py
+++ cactus/views.py
@@ -1,4 +1,4 @@
-import tweepy, json
+import tweepy
 from tweepy import OAuthHandler
 from instaLooter import InstaLooter
 import pdb
@@ -15,23 +15,6 @@ def twitter():
 
     api = tweepy.API(auth)
 
-    #def process_or_store(tweet):
-        #print(json.dumps(tweet))
-
-    #for tweet in tweepy.Cursor(api.user_timeline).items():
-        #process_or_store(tweet._json)
-
-    #for status in tweepy.Cursor(api.home_timeline).items(10):
-        #print(status.text)
-
-    #for status in tweepy.Cursor(api.home_timeline).items(10):
-        # Process a single status
-        #process_or_store(status._json)
-
-    #for friend in tweepy.Cursor(api.friends).items():
-        #process_or_store(friend._json)
-
-    #if __name__=="__main__":
     user = api.get_user(input("Please enter the twitter username: "))
     return (api.me().name)
     return (api.me().description)
@@ -44,16 +27,12 @@ def instagram():
 
     looter = InstaLooter(profile=username)
 
-    # with open("outimagesfile.txt", "w") as output:
-    # pdb.set_trace()
     for media in looter.medias():
-        url = looter.get_post_info(media['code']['edge_media_to_caption'])
+        text = looter.get_post_info(media['code'])['edge_media_to_caption']['edges'][0]['node']['text'].split('#')[0]
+        hashtags = looter.get_post_info(media['code'])['edge_media_to_caption']['edges'][0]['node']['text'].split('#')[
+                   1:]
         k += 1
         if k > 2:
             break
-            # else:
-            # url = media['display_src']
-            # k+=1
-        # if k==3:
-        # break output.write
-        print("{}\n".format(url))
+        print("{}\n".format(text))
+        print("{}\n".format(hashtags))
